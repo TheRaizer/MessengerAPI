5823023dc9dce5a088db478f3b1c2269704dfbb2
Aidan Fu <aidan.fu000@gmail.com>
2022-11-16 23:54:47 -0400

    add test for successful sending of friend request

--- tests/routers/test_friends_router.py
+++ tests/routers/test_friends_router.py
@@ -8,6 +8,9 @@ from _submodules.messenger_utils.messenger_schemas.schema import database_sessio
 from _submodules.messenger_utils.messenger_schemas.schema.friendship_schema import (
     FriendshipSchema,
 )
+from _submodules.messenger_utils.messenger_schemas.schema.friendship_status_schema import (
+    FriendshipStatusSchema,
+)
 from _submodules.messenger_utils.messenger_schemas.schema.user_schema import UserSchema
 from messenger.constants.friendship_status_codes import FriendshipStatusCode
 from messenger.fastApi import app
@@ -52,6 +55,73 @@ def override_database_session():
 client = TestClient(app)
 app.dependency_overrides[get_current_active_user] = override_get_current_active_user
 app.dependency_overrides[database_session] = override_database_session
+
+
+@freeze_time("2022-11-06")
+@pytest.mark.parametrize("username", ["Some_username12", "_another_user", "hi_im_user"])
+@patch("messenger.routers.friends.FriendshipHandler")
+@patch("messenger.routers.friends.UserHandler")
+@patch("messenger.routers.friends.FriendshipSchema")
+@patch("messenger.routers.friends.FriendshipStatusSchema")
+def test_send_friendship_request(
+    FriendshipStatusSchemaMock: MagicMock,
+    FriendshipSchemaMock: MagicMock,
+    UserHandlerMock: MagicMock,
+    FriendshipHandlerMock: MagicMock,
+    username: str,
+):
+    addressee = UserSchema(
+        user_id=1233, username=username, email="some-email", password_hash="some-hash"
+    )
+    UserHandlerMock.return_value.get_user.return_value = addressee
+    FriendshipHandlerMock.return_value.get_friendship_bidirectional_query.return_value = (
+        None
+    )
+
+    new_friendship_mock = FriendshipSchema(
+        requester_id=current_active_user.user_id,
+        addressee_id=addressee.user_id,
+        created_date_time=datetime.now(),
+    )
+    new_friendship_status_mock = FriendshipStatusSchema(
+        requester_id=current_active_user.user_id,
+        addressee_id=addressee.user_id,
+        specified_date_time=datetime.now(),
+        status_code_id=FriendshipStatusCode.REQUESTED.value,
+        specifier_id=current_active_user.user_id,
+    )
+    FriendshipSchemaMock.return_value = new_friendship_mock
+    FriendshipStatusSchemaMock.return_value = new_friendship_status_mock
+
+    response = client.post(f"/friends/requests/send-request?username={username}")
+
+    assert response.status_code == 201
+
+    FriendshipSchemaMock.assert_called_once_with(
+        requester_id=current_active_user.user_id,
+        addressee_id=addressee.user_id,
+        created_date_time=datetime.now(),
+    )
+
+    FriendshipStatusSchemaMock.assert_called_once_with(
+        requester_id=current_active_user.user_id,
+        addressee_id=addressee.user_id,
+        specified_date_time=datetime.now(),
+        status_code_id=FriendshipStatusCode.REQUESTED.value,
+        specifier_id=current_active_user.user_id,
+    )
+
+    assert session_mock.add.call_count == 2
+
+    session_mock.add.assert_any_call(FriendshipSchemaMock.return_value)
+    session_mock.add.assert_any_call(FriendshipStatusSchemaMock.return_value)
+    session_mock.commit.assert_called_once()
+
+    assert response.json() == {
+        "requester_id": new_friendship_mock.requester_id,
+        "addressee_id": new_friendship_mock.addressee_id,
+        "created_date_time": "2022-11-06T00:00:00",  # equivalent to the frozen time
+    }
 
 
 def test_get_friendship_requests_sent():
