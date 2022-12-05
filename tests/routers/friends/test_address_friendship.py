6ea00dfaec120465cee8dbea51a35ccd125afbf1
Aidan Fu <aidan.fu000@gmail.com>
2022-12-05 15:44:20 -0500

    add tests for addressing friendship routes, when blocked, when already addressed, when no friendship, when no user

--- tests/routers/friends/test_address_friendship.py
+++ tests/routers/friends/test_address_friendship.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 from operator import attrgetter
 from typing import Tuple
-from unittest.mock import MagicMock, patch
 from freezegun import freeze_time
 import pytest
 from fastapi.testclient import TestClient
@@ -153,3 +152,180 @@ class TestAddressingFriendship:
         )
         assert added_friendship_status.specified_date_time == datetime.now()
         assert response.status_code == 201
+
+    @pytest.mark.parametrize(
+        "username",
+        valid_usernames,
+    )
+    def test_when_no_user_found(
+        self,
+        username: str,
+        client: Tuple[TestClient, UserSchema],
+    ):
+        # no requester user was added to the DB, so the request should fail
+        (test_client, _) = client
+
+        response = test_client.post(
+            f"/friends/requests/decline?requester_username={username}"
+        )
+
+        assert response.status_code == 404
+        assert response.json() == {"detail": "no such user exists"}
+
+        response = test_client.post(
+            f"/friends/requests/accept?requester_username={username}"
+        )
+
+        assert response.status_code == 404
+        assert response.json() == {"detail": "no such user exists"}
+
+    @pytest.mark.parametrize(
+        "username, email, password",
+        zip(valid_usernames, valid_emails, valid_passwords),
+    )
+    def test_when_no_friendship_found(
+        self,
+        username: str,
+        password: str,
+        email: str,
+        client: Tuple[TestClient, UserSchema],
+        session: Session,
+    ):
+        (test_client, current_active_user) = client
+
+        friendship_requester = UserSchema(
+            user_id=current_active_user.user_id + 1,
+            username=username,
+            password_hash=password,
+            email=email,
+        )
+
+        session.add(friendship_requester)
+        session.commit()
+
+        response = test_client.post(
+            f"/friends/requests/decline?requester_username={username}"
+        )
+
+        assert response.status_code == 404
+        assert response.json() == {"detail": "friendship was not found"}
+
+        response = test_client.post(
+            f"/friends/requests/accept?requester_username={username}"
+        )
+
+        assert response.status_code == 404
+        assert response.json() == {"detail": "friendship was not found"}
+
+    @freeze_time(FROZEN_DATE)
+    @pytest.mark.parametrize(
+        "username, email, password",
+        zip(valid_usernames, valid_emails, valid_passwords),
+    )
+    def test_when_friendship_already_addressed(
+        self,
+        username: str,
+        password: str,
+        email: str,
+        client: Tuple[TestClient, UserSchema],
+        session: Session,
+    ):
+        (test_client, current_active_user) = client
+
+        (friendship, friendship_requester) = self.initialize_friendship_request(
+            session, current_active_user, username, email, password
+        )
+
+        friendship.statuses.insert(
+            0,
+            FriendshipStatusSchema(
+                requester_id=friendship_requester.user_id,
+                addressee_id=current_active_user.user_id,
+                specifier_id=current_active_user.user_id,
+                status_code_id=FriendshipStatusCode.ACCEPTED.value,
+                specified_date_time=datetime.now() + timedelta(days=10),
+            ),
+        )
+
+        response = test_client.post(
+            f"/friends/requests/decline?requester_username={username}"
+        )
+
+        assert response.status_code == 400
+        assert response.json() == {"detail": "friend request already addressed"}
+
+        response = test_client.post(
+            f"/friends/requests/accept?requester_username={username}"
+        )
+
+        assert response.status_code == 400
+        assert response.json() == {"detail": "friend request already addressed"}
+
+        friendship.statuses.insert(
+            0,
+            FriendshipStatusSchema(
+                requester_id=friendship_requester.user_id,
+                addressee_id=current_active_user.user_id,
+                specifier_id=current_active_user.user_id,
+                status_code_id=FriendshipStatusCode.DECLINED.value,
+                specified_date_time=datetime.now() + timedelta(days=11),
+            ),
+        )
+
+        response = test_client.post(
+            f"/friends/requests/decline?requester_username={username}"
+        )
+
+        assert response.status_code == 400
+        assert response.json() == {"detail": "friend request already addressed"}
+
+        response = test_client.post(
+            f"/friends/requests/accept?requester_username={username}"
+        )
+
+        assert response.status_code == 400
+        assert response.json() == {"detail": "friend request already addressed"}
+
+    @freeze_time(FROZEN_DATE)
+    @pytest.mark.parametrize(
+        "username, email, password",
+        zip(valid_usernames, valid_emails, valid_passwords),
+    )
+    def test_when_friendship_blocked(
+        self,
+        username: str,
+        password: str,
+        email: str,
+        client: Tuple[TestClient, UserSchema],
+        session: Session,
+    ):
+        (test_client, current_active_user) = client
+
+        (friendship, friendship_requester) = self.initialize_friendship_request(
+            session, current_active_user, username, email, password
+        )
+
+        friendship.statuses.insert(
+            0,
+            FriendshipStatusSchema(
+                requester_id=friendship_requester.user_id,
+                addressee_id=current_active_user.user_id,
+                specifier_id=current_active_user.user_id,
+                status_code_id=FriendshipStatusCode.BLOCKED.value,
+                specified_date_time=datetime.now() + timedelta(days=10),
+            ),
+        )
+
+        response = test_client.post(
+            f"/friends/requests/decline?requester_username={username}"
+        )
+
+        assert response.status_code == 400
+        assert response.json() == {"detail": "friendship is blocked"}
+
+        response = test_client.post(
+            f"/friends/requests/accept?requester_username={username}"
+        )
+
+        assert response.status_code == 400
+        assert response.json() == {"detail": "friendship is blocked"}
