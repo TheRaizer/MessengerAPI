9f8a6bd2234b5681f92ca87843e697f97f9cbb63
Aidan Fu <aidan.fu000@gmail.com>
2022-12-05 16:22:29 -0500

    add more in depth test cases for accepting and declining friendship request routes

---
+++ tests/routers/friends/address_friendship/assertions.py
@@ -0,0 +1,189 @@
+from datetime import datetime, timedelta
+from operator import attrgetter
+from typing import Tuple
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+from _submodules.messenger_utils.messenger_schemas.schema.friendship_status_schema import (
+    FriendshipStatusSchema,
+)
+from _submodules.messenger_utils.messenger_schemas.schema.user_schema import (
+    UserSchema,
+)
+from messenger.constants.friendship_status_codes import FriendshipStatusCode
+from tests.routers.friends.address_friendship import (
+    initialize_friendship_request,
+)
+
+
+def assert_addressing_adds_new_friendship_status_to_db(
+    url: str,
+    username: str,
+    email: str,
+    password: str,
+    client: Tuple[TestClient, UserSchema],
+    session: Session,
+    expected_status: FriendshipStatusCode,
+):
+    (test_client, current_active_user) = client
+    (friendship, friendship_requester) = initialize_friendship_request(
+        session,
+        current_active_user,
+        username,
+        email,
+        password,
+    )
+
+    test_client.post(url)
+
+    session.refresh(friendship)
+
+    added_friendship_status = max(
+        friendship.statuses,
+        key=attrgetter("specified_date_time"),
+    )
+
+    assert added_friendship_status.status_code_id == expected_status.value
+    assert added_friendship_status.requester_id == friendship_requester.user_id
+    assert added_friendship_status.addressee_id == current_active_user.user_id
+    assert added_friendship_status.specifier_id == current_active_user.user_id
+    assert added_friendship_status.specified_date_time == datetime.now()
+
+
+def assert_addressing_produces_201_status_code(
+    url: str,
+    username: str,
+    email: str,
+    password: str,
+    client: Tuple[TestClient, UserSchema],
+    session: Session,
+):
+    (test_client, current_active_user) = client
+
+    initialize_friendship_request(
+        session,
+        current_active_user,
+        username,
+        email,
+        password,
+    )
+
+    response = test_client.post(url)
+
+    assert response.status_code == 201
+
+
+def assert_addressing_fails_when_user_not_found(
+    url: str,
+    client: Tuple[TestClient, UserSchema],
+):
+    # no requester user was added to the DB, so the request should fail
+    (test_client, _) = client
+
+    response = test_client.post(url)
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": "no such user exists"}
+
+
+def assert_addressing_fails_when_friendship_not_found(
+    url: str,
+    username: str,
+    password: str,
+    email: str,
+    client: Tuple[TestClient, UserSchema],
+    session: Session,
+):
+    (test_client, current_active_user) = client
+
+    friendship_requester = UserSchema(
+        user_id=current_active_user.user_id + 1,
+        username=username,
+        password_hash=password,
+        email=email,
+    )
+
+    session.add(friendship_requester)
+    session.commit()
+
+    response = test_client.post(url)
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": "friendship was not found"}
+
+
+def assert_addressing_fails_when_friendship_already_addressed(
+    url: str,
+    username: str,
+    password: str,
+    email: str,
+    client: Tuple[TestClient, UserSchema],
+    session: Session,
+):
+    (test_client, current_active_user) = client
+
+    (friendship, friendship_requester) = initialize_friendship_request(
+        session, current_active_user, username, email, password
+    )
+
+    friendship.statuses.insert(
+        0,
+        FriendshipStatusSchema(
+            requester_id=friendship_requester.user_id,
+            addressee_id=current_active_user.user_id,
+            specifier_id=current_active_user.user_id,
+            status_code_id=FriendshipStatusCode.ACCEPTED.value,
+            specified_date_time=datetime.now() + timedelta(days=10),
+        ),
+    )
+
+    response = test_client.post(url)
+
+    assert response.status_code == 400
+    assert response.json() == {"detail": "friend request already addressed"}
+
+    friendship.statuses.insert(
+        0,
+        FriendshipStatusSchema(
+            requester_id=friendship_requester.user_id,
+            addressee_id=current_active_user.user_id,
+            specifier_id=current_active_user.user_id,
+            status_code_id=FriendshipStatusCode.DECLINED.value,
+            specified_date_time=datetime.now() + timedelta(days=11),
+        ),
+    )
+
+    response = test_client.post(url)
+
+    assert response.status_code == 400
+    assert response.json() == {"detail": "friend request already addressed"}
+
+
+def assert_addressing_fails_when_friendship_blocked(
+    url: str,
+    username: str,
+    password: str,
+    email: str,
+    client: Tuple[TestClient, UserSchema],
+    session: Session,
+):
+    (test_client, current_active_user) = client
+
+    (friendship, friendship_requester) = initialize_friendship_request(
+        session, current_active_user, username, email, password
+    )
+
+    friendship.statuses.insert(
+        0,
+        FriendshipStatusSchema(
+            requester_id=friendship_requester.user_id,
+            addressee_id=current_active_user.user_id,
+            specifier_id=current_active_user.user_id,
+            status_code_id=FriendshipStatusCode.BLOCKED.value,
+            specified_date_time=datetime.now() + timedelta(days=10),
+        ),
+    )
+
+    response = test_client.post(url)
+
+    assert response.status_code == 400
+    assert response.json() == {"detail": "friendship is blocked"}
---
+++ tests/routers/friends/address_friendship/test_accept_friendship.py
@@ -0,0 +1,149 @@
+from typing import Tuple
+from freezegun import freeze_time
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+from _submodules.messenger_utils.messenger_schemas.schema.user_schema import (
+    UserSchema,
+)
+from messenger.constants.friendship_status_codes import FriendshipStatusCode
+from tests.routers.friends.address_friendship.assertions import (
+    assert_addressing_adds_new_friendship_status_to_db,
+    assert_addressing_fails_when_friendship_already_addressed,
+    assert_addressing_fails_when_friendship_blocked,
+    assert_addressing_fails_when_friendship_not_found,
+    assert_addressing_fails_when_user_not_found,
+    assert_addressing_produces_201_status_code,
+)
+from tests.routers.friends.conftest import FROZEN_DATE
+from tests.conftest import (
+    valid_usernames,
+    valid_emails,
+    valid_passwords,
+)
+
+
+class TestAcceptFriendshipRequest:
+    @freeze_time(FROZEN_DATE)
+    @pytest.mark.parametrize(
+        "username, email, password",
+        zip(valid_usernames, valid_emails, valid_passwords),
+    )
+    def test_it_adds_new_friendship_status_to_db(
+        self,
+        username: str,
+        email: str,
+        password: str,
+        client: Tuple[TestClient, UserSchema],
+        session: Session,
+    ):
+        assert_addressing_adds_new_friendship_status_to_db(
+            f"/friends/requests/accept?requester_username={username}",
+            username,
+            email,
+            password,
+            client,
+            session,
+            FriendshipStatusCode.ACCEPTED
+        )
+
+    @freeze_time(FROZEN_DATE)
+    @pytest.mark.parametrize(
+        "username, email, password",
+        zip(valid_usernames, valid_emails, valid_passwords),
+    )
+    def test_response_status_code(
+        self,
+        username: str,
+        email: str,
+        password: str,
+        client: Tuple[TestClient, UserSchema],
+        session: Session,
+    ):
+        assert_addressing_produces_201_status_code(
+            f"/friends/requests/accept?requester_username={username}",
+            username,
+            email,
+            password,
+            client,
+            session,
+        )
+
+    @pytest.mark.parametrize(
+        "username",
+        valid_usernames,
+    )
+    def test_when_no_user_found(
+        self,
+        username: str,
+        client: Tuple[TestClient, UserSchema],
+    ):
+        assert_addressing_fails_when_user_not_found(
+            f"/friends/requests/accept?requester_username={username}",
+            client,
+        )
+
+    @pytest.mark.parametrize(
+        "username, email, password",
+        zip(valid_usernames, valid_emails, valid_passwords),
+    )
+    def test_when_no_friendship_found(
+        self,
+        username: str,
+        password: str,
+        email: str,
+        client: Tuple[TestClient, UserSchema],
+        session: Session,
+    ):
+        assert_addressing_fails_when_friendship_not_found(
+            f"/friends/requests/accept?requester_username={username}",
+            username,
+            password,
+            email,
+            client,
+            session,
+        )
+
+    @freeze_time(FROZEN_DATE)
+    @pytest.mark.parametrize(
+        "username, email, password",
+        zip(valid_usernames, valid_emails, valid_passwords),
+    )
+    def test_when_friendship_already_addressed(
+        self,
+        username: str,
+        password: str,
+        email: str,
+        client: Tuple[TestClient, UserSchema],
+        session: Session,
+    ):
+        assert_addressing_fails_when_friendship_already_addressed(
+            f"/friends/requests/accept?requester_username={username}",
+            username,
+            password,
+            email,
+            client,
+            session,
+        )
+
+    @freeze_time(FROZEN_DATE)
+    @pytest.mark.parametrize(
+        "username, email, password",
+        zip(valid_usernames, valid_emails, valid_passwords),
+    )
+    def test_when_friendship_blocked(
+        self,
+        username: str,
+        password: str,
+        email: str,
+        client: Tuple[TestClient, UserSchema],
+        session: Session,
+    ):
+        assert_addressing_fails_when_friendship_blocked(
+            f"/friends/requests/accept?requester_username={username}",
+            username,
+            password,
+            email,
+            client,
+            session,
+        )
