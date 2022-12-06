5da0af96b0d30900f4551599c21c4026710b47e9
Aidan Fu <aidan.fu000@gmail.com>
2022-12-06 16:02:38 -0500

    refactor test_retrieve_friendships.py tests to use test_db

--- tests/routers/friends/test_retrieve_friendships.py
+++ tests/routers/friends/test_retrieve_friendships.py
@@ -1,38 +1,109 @@
+from datetime import datetime
+from typing import Tuple
 from fastapi.testclient import TestClient
 from _submodules.messenger_utils.messenger_schemas.schema.friendship_schema import (
     FriendshipSchema,
 )
-from .conftest import (
-    friendship_requests_recieved,
-    friendship_requests_sent,
+from _submodules.messenger_utils.messenger_schemas.schema.user_schema import (
+    UserSchema,
 )
 
 
-def test_get_friendship_requests_sent(client: TestClient):
-    response = client.get("/friends/requests/sent")
+def test_get_friendship_requests_sent_produces_200(
+    client: Tuple[TestClient, UserSchema]
+):
+    (test_client, _) = client
+
+    response = test_client.get("/friends/requests/sent")
     assert response.status_code == 200
 
-    arr = response.json()
 
-    assert len(friendship_requests_sent) == len(arr)
+def test_get_friendship_requests_sent(client: Tuple[TestClient, UserSchema]):
+    (test_client, current_active_user) = client
 
-    for res_friendship_dict, friendship_expected in zip(
-        arr, friendship_requests_sent
-    ):
-        friendship = FriendshipSchema(**res_friendship_dict)
-        assert friendship == friendship_expected
+    expected_friendship_requests_sent = [
+        FriendshipSchema(
+            requester_id=current_active_user.user_id,
+            addressee_id=2,
+            created_date_time=datetime.now(),
+        ),
+        FriendshipSchema(
+            requester_id=current_active_user.user_id,
+            addressee_id=5,
+            created_date_time=datetime.now(),
+        ),
+        FriendshipSchema(
+            requester_id=current_active_user.user_id,
+            addressee_id=11,
+            created_date_time=datetime.now(),
+        ),
+    ]
 
+    current_active_user.friend_requests_sent = expected_friendship_requests_sent
 
-def test_get_friendship_requests_recieved(client: TestClient):
-    response = client.get("/friends/requests/recieved")
+    response = test_client.get("/friends/requests/sent")
     assert response.status_code == 200
 
-    arr = response.json()
+    friendship_requests_sent = response.json()
 
-    assert len(friendship_requests_recieved) == len(arr)
+    assert len(expected_friendship_requests_sent) == len(
+        friendship_requests_sent
+    )
 
-    for res_friendship_dict, friendship_expected in zip(
-        arr, friendship_requests_recieved
+    for friendship_request_sent, friendship_request_expected in zip(
+        friendship_requests_sent, expected_friendship_requests_sent
     ):
-        friendship = FriendshipSchema(**res_friendship_dict)
-        assert friendship == friendship_expected
+        friendship = FriendshipSchema(**friendship_request_sent)
+        assert friendship == friendship_request_expected
+
+
+def test_get_friendship_requests_recieved_produces_200(
+    client: Tuple[TestClient, UserSchema]
+):
+    (test_client, _) = client
+
+    response = test_client.get("/friends/requests/recieved")
+    assert response.status_code == 200
+
+
+def test_get_friendship_requests_recieved(
+    client: Tuple[TestClient, UserSchema]
+):
+    (test_client, current_active_user) = client
+
+    expected_friendship_requests_recieved = [
+        FriendshipSchema(
+            requester_id=3,
+            addressee_id=current_active_user.user_id,
+            created_date_time=datetime.now(),
+        ),
+        FriendshipSchema(
+            requester_id=8,
+            addressee_id=current_active_user.user_id,
+            created_date_time=datetime.now(),
+        ),
+        FriendshipSchema(
+            requester_id=896,
+            addressee_id=current_active_user.user_id,
+            created_date_time=datetime.now(),
+        ),
+    ]
+
+    current_active_user.friend_requests_recieved = (
+        expected_friendship_requests_recieved
+    )
+
+    response = test_client.get("/friends/requests/recieved")
+    assert response.status_code == 200
+
+    friendship_requests_recieved = response.json()
+
+    assert len(friendship_requests_recieved) == len(
+        expected_friendship_requests_recieved
+    )
+
+    for friendship_request_recieved, friendship_request_expected in zip(
+        friendship_requests_recieved, expected_friendship_requests_recieved
+    ):
+        friendship = FriendshipSchema(**friendship_request_recieved)
+        assert friendship == friendship_request_expected
