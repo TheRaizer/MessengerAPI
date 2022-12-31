5e2622d8f30a664d7a613e58a7b706785d6b8a52
Aidan Fu <aidan.fu000@gmail.com>
2022-12-31 17:28:30 -0500

    remove unneeded cors from socketio that will be handled with fastapi

--- messenger/sockets/__init__.py
+++ messenger/sockets/__init__.py
@@ -1,8 +1,6 @@
 import socketio
-from messenger.settings import origins
 
 sio = socketio.AsyncServer(
-    cors_allowed_origins=origins,
     async_mode="asgi",
     logger=True,
     engineio_logger=True,
