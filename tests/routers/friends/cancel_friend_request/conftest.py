f302d05a0268cf182a8fa0f306be21fddab8737c
Aidan Fu <aidan.fu000@gmail.com>
2023-01-24 14:02:32 -0500

    add friendship request cancellation route tests

--- tests/routers/friends/cancel_friend_request/conftest.py
+++ tests/routers/friends/cancel_friend_request/conftest.py
@@ -13,7 +13,7 @@ produce_400_data = (
                 (42, FriendshipStatusCode.ACCEPTED),
                 (4, FriendshipStatusCode.BLOCKED),
             ],
-            [4],
+            4,
             generate_username(4),
         ),
         (
@@ -25,7 +25,7 @@ produce_400_data = (
                 (6, FriendshipStatusCode.BLOCKED),
                 (7, FriendshipStatusCode.BLOCKED),
             ],
-            [2],
+            2,
             generate_username(2),
         ),
         (
@@ -38,8 +38,45 @@ produce_400_data = (
                 (2, FriendshipStatusCode.DECLINED),
                 (6, FriendshipStatusCode.ACCEPTED),
             ],
-            [32],
+            32,
             generate_username(32),
+        ),
+    ],
+)
+
+
+cancellable_friend_request_data = (
+    "friend_data, addressee_user_id, addressee_username",
+    [
+        (
+            [
+                (2, FriendshipStatusCode.ACCEPTED),
+                (4, FriendshipStatusCode.REQUESTED),
+                (2, FriendshipStatusCode.BLOCKED),
+                (42, FriendshipStatusCode.REQUESTED),
+                (4, FriendshipStatusCode.BLOCKED),
+            ],
+            42,
+            generate_username(42),
+        ),
+        (
+            [
+                (3, FriendshipStatusCode.BLOCKED),
+                (5, FriendshipStatusCode.BLOCKED),
+                (7, FriendshipStatusCode.REQUESTED),
+            ],
+            7,
+            generate_username(7),
+        ),
+        (
+            [
+                (2, FriendshipStatusCode.BLOCKED),
+                (9, FriendshipStatusCode.ACCEPTED),
+                (3, FriendshipStatusCode.DECLINED),
+                (6, FriendshipStatusCode.REQUESTED),
+            ],
+            6,
+            generate_username(6),
         ),
     ],
 )
--- tests/routers/friends/cancel_friend_request/test_cancel_friendship_request.py
+++ tests/routers/friends/cancel_friend_request/test_cancel_friendship_request.py
@@ -5,6 +5,12 @@ from sqlalchemy.orm import Session
 from messenger_schemas.schema.user_schema import (
     UserSchema,
 )
+from messenger_schemas.schema.friendship_schema import (
+    FriendshipSchema,
+)
+from messenger_schemas.schema.friendship_status_schema import (
+    FriendshipStatusSchema,
+)
 from messenger.constants.friendship_status_codes import FriendshipStatusCode
 
 from tests.conftest import (
@@ -14,12 +20,11 @@ from tests.conftest import (
 from tests.routers.friends.conftest import add_friendships
 from tests.routers.friends.cancel_friend_request.conftest import (
     produce_400_data,
+    cancellable_friend_request_data,
 )
 
 
 class TestCancelFriendshipRequest:
-    # TODO: test it cancels a friendship request by deleting it
-
     def test_produces_201_when_successful(
         self,
         client: Tuple[TestClient, UserSchema],
@@ -56,7 +61,7 @@ class TestCancelFriendshipRequest:
     def test_produces_400_when_friendship_does_not_have_requested_latest_status(
         self,
         friend_data: List[Tuple[int, FriendshipStatusCode]],
-        addressee_user_id: List[int],
+        addressee_user_id: int,
         addressee_username: str,
         client: Tuple[TestClient, UserSchema],
         session: Session,
@@ -67,7 +72,7 @@ class TestCancelFriendshipRequest:
 
         add_friendships(
             friend_data,
-            addressee_user_id,
+            [addressee_user_id],
             current_active_user.user_id,
             session,
         )
@@ -76,3 +81,54 @@ class TestCancelFriendshipRequest:
             f"/friends/requests/cancel?request_addressee_username={addressee_username}"
         )
         assert response.status_code == 400
+
+    @pytest.mark.parametrize(
+        cancellable_friend_request_data[0], cancellable_friend_request_data[1]
+    )
+    def test_successfully_cancels_friend_request(
+        self,
+        friend_data: List[Tuple[int, FriendshipStatusCode]],
+        addressee_user_id: int,
+        addressee_username: str,
+        client: Tuple[TestClient, UserSchema],
+        session: Session,
+    ):
+        (test_client, current_active_user) = client
+
+        add_initial_friendship_status_codes(session)
+
+        add_friendships(
+            friend_data,
+            [addressee_user_id],
+            current_active_user.user_id,
+            session,
+        )
+
+        response = test_client.post(
+            f"/friends/requests/cancel?request_addressee_username={addressee_username}"
+        )
+
+        assert response.status_code == 201
+
+        # friendship and its statuses should be removed from the database
+        friendship = (
+            session.query(FriendshipSchema)
+            .filter(
+                FriendshipSchema.addressee_id == addressee_user_id,
+                FriendshipSchema.requester_id == current_active_user.user_id,
+            )
+            .first()
+        )
+
+        friendship_statuses = (
+            session.query(FriendshipStatusSchema)
+            .filter(
+                FriendshipStatusSchema.addressee_id == addressee_user_id,
+                FriendshipStatusSchema.requester_id
+                == current_active_user.user_id,
+            )
+            .all()
+        )
+
+        assert friendship is None
+        assert len(friendship_statuses) == 0
